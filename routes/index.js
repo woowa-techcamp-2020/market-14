646f0fa1fe06c69cfa630620354c1bb3e080436e
Wudy <wudy343@gmail.com>
2020-07-08 16:53:54 +0900

    [#3] 로그인 마크업 및 라우트 연결

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('./pages/mainPage', { title: 'Express' });
+  res.render('./pages/mainPage', { title: '배민상회::메인' });
+});
+
+router.get('/signin', (req, res) => {
+  res.render('./pages/signin', { title: '배민상회::로그인' });
 });
 
 module.exports = router;
