841a79ed69f57c3a66a75542c4f0126bf50ffb3b
Wudy <wudy343@gmail.com>
2020-07-09 14:37:35 +0900

    [#5] 회원가입 화면 마크업 및 스타일 작업
    이외 작업, 라우트 추가

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,8 @@ router.get('/signin', (req, res) => {
   res.render('./pages/signin', { title: '로그인 - 배민상회' });
 });
 
+router.get('/signup', (req, res) => {
+  res.render('./pages/signup', { title: '회원가입 - 배민상회' });
+});
+
 module.exports = router;
